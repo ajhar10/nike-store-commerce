d26058a78dabddf53021b379d1c0bf92c3899deb
ajhar10 <ajharrimon90@gmail.com>
2022-11-06 16:12:46 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="bg-black text-red-500 text-4xl font-bold">
+        Welcome to Nike Store Project
+      </h1>
+    </div>
+  );
+};
+
+export default App;
